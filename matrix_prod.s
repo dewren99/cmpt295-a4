ee8ffd194d5adf7302e3d4f0fb40812da73dc6d0
devrendi <devrendi@asb9838nu-d08.csil.sfu.ca>
2019-10-17 22:51:02 -0700

    final version

--- matrix_prod.s
+++ matrix_prod.s
@@ -1,55 +1,53 @@
 
     .globl matrix_prod
-matrix_prod:            # void matrix_prod(void *A, void *B, void *C, int n);
+matrix_prod:    # void matrix_prod(void *A, void *B, void *C, int n);
 	pushq   %rbp
 	movq    %rsp, %rbp # prologue
-    movq    %rdi, -48(%rbp)  # A
-    movq    %rsi, -16(%rbp) # B
-    movq    %rdx, -24(%rbp) # C
-    movl    %ecx, -32(%rbp) # n
-    movl    $0,   -36(%rbp) # i
 
-outerLoop:
-    movl   -36(%rbp), %eax
-    cmpl     %eax, -32(%rbp)
+    movl    $0,   -16(%rbp) # i
+    movl    %ecx, -24(%rbp) # n
+    movq    %rdi, -32(%rbp) # A
+    movq    %rsi, -40(%rbp) # B
+    movq    %rdx, -48(%rbp) # C
+
+outerLoop:  # for i from 0 to n-1
+    movl   -16(%rbp), %eax
+    cmpl     %eax, -24(%rbp)
     je      endl
-    movl    $0, -40(%rbp)   # j
+    movl    $0, -20(%rbp)   # j
 
-innerLoop:
-    movl    -40(%rbp), %eax    
-    cmpl     %eax, -32(%rbp)
+innerLoop:  # for j from 0 to n-1  
+    movl    -20(%rbp), %eax    
+    cmpl     %eax, -24(%rbp)
     je      innerEndl
     
+    # Putting saved values inside arg registers and calling dot_prod
+    movq    -32(%rbp), %rdi # A
+    movq    -40(%rbp), %rsi # B
+    movl    -24(%rbp), %edx # n
+    movl    -16(%rbp), %ecx # i
+    movl    -20(%rbp), %r8d # j
+    call    dot_prod
 
-    movq    -48(%rbp), %rdi  # A
-    movq    -16(%rbp), %rsi # B
-    movl    -32(%rbp), %edx # n
-    movl    -36(%rbp), %ecx # i
-    movl    -40(%rbp), %r8d # j
-    call    dot_prod # rax = # long dot_prod(void *A, void *B, int n, int i, int j);
-    movq    -48(%rbp), %rdi  # A 
-    movq    %rax, -56(%rbp)
+    # moving return value of dot_prod to rdi, 17 to rsi and calling mod
     movq    %rax, %rdi
     movl    $17, %esi
     call    mod
 
-    movq    -48(%rbp), %rdi  # A
-
-    movl    %eax, %edx # rdx = rax = mod()
-    movl    -36(%rbp), %eax # eax = i
-    imull   -32(%rbp), %eax # eax = i * n
-    addl    -40(%rbp), %eax  # eax = i*n+j
+    movl    %eax, %edx # saving return value of mod func for later
     
-    addq    -24(%rbp), %rax # rax = rax + rcx = C + i*n+j
-    mov    %dl, (%rax) # C[i][j] = rdx
+    movl    -16(%rbp), %eax
+    imull   -24(%rbp), %eax
+    addl    -20(%rbp), %eax  # eax = value of the current index (i*n+j)
+    
+    addq    -48(%rbp), %rax # rax pointing to current index of C
+    mov    %dl, (%rax)      # place the value inside C[i][j]
 
-    movq    -48(%rbp), %rdi  # A
-
-    add     $1, -40(%rbp)
+    add     $1, -20(%rbp)
     jmp     innerLoop
 
 innerEndl:
-     add     $1, -36(%rbp)
+     add     $1, -16(%rbp)
      jmp outerLoop
 
 endl:
